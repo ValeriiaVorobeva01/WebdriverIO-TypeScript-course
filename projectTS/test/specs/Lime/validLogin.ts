a4d86adb7dc04f4a9484db5daf71edd38e7e0086
ValeriiaVorobeva0 <bulletlove01@gmail.com>
2023-12-30 13:17:26 +0700

    commit 30.12

--- projectTS/test/specs/Lime/validLogin.ts
+++ projectTS/test/specs/Lime/validLogin.ts
@@ -1,20 +1,20 @@
 describe ('Login valid', () => {
     it('login', async () => {
         browser.url('https://cabinet-ru-lime-vvorobeva.te.lime.local/login')
-        // await browser.pause(1000)
-        // // заполняем поле
-        // await $('#login-mobile').setValue('79953512245')
-        // // проверяем значение в поле
-        // console.log('getvalue - ' + await (await $('#login-mobile')).getValue())
-        // expect(await $('#login-mobile').getValue()).toEqual('+7(995) 351-2245')
-        // // заполняем поле
-        // await browser.pause(1000)
-        // await (await $("input[id='login-password']")).setValue('123qwe')
+        await browser.pause(1000)
+        // заполняем поле
+        await $('#login-mobile').setValue('79953512245')
+        // проверяем значение в поле
+        console.log('getvalue - ' + await (await $('#login-mobile')).getValue())
+        expect(await $('#login-mobile').getValue()).toEqual('+7(995) 351-2245')
+        // заполняем поле
+        await browser.pause(1000)
+        await (await $("input[id='login-password']")).setValue('123qwe')
 
         await browser.pause(1000)
-        // await $('button').click()
+        await $('').click()
 
-        // await browser.pause(3000)       
-        // await expect('<h1>').toHaveText('Мой заём')
+        await browser.pause(3000)       
+        await expect('<h1>').toHaveText('Мой заём')
     })
 })
